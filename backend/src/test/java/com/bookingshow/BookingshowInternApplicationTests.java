9c7f2d8f2d0fbb65134fab6f9a8d96e0cb64ea02
DoctorFear <thaisonx2602@gmail.com>
2026-06-17 13:10:29 +0700

    feat: create Spring Boot backend skeleton with Maven

---
+++ backend/src/test/java/com/bookingshow/BookingshowInternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookingshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingshowInternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
